6a442f39e007a973fd9091b728b0d4462e766624
Alexander Fedorov <rasmygens@gmail.com>
2022-11-06 03:47:52 +0300

    +GET with AXIOS

--- services/api.service.js
+++ services/api.service.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import https from 'https';
 import { getKeyValue, TOKEN_DICT } from './storage.service.js';
 
@@ -7,22 +8,16 @@ const getWeather = async (city) => {
 		throw new Error('Не задан ключ API, задайте его через -t [API_KEY]');
 	}
 
-	//const url = `api.openweathermap.org/data/2.5/weather?q=${city}& appid=${API} `;
-	const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-	url.searchParams.append('q', city);
-	url.searchParams.append('appid', token);
-	url.searchParams.append('lang', 'ru');
-	url.searchParams.append('units', 'metric');
-	https.get(url, response => {
-		let res = '';
-		response.on('data', chunk => {
-			res += chunk;
-		});
-
-		response.on('end', () => {
-			console.log(res);
-		});
+	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+		params: {
+			q: city,
+			appid: token,
+			lang: 'ru',
+			units: 'metric'
+		}
 	});
+
+	console.log(data);
 };
 
 export { getWeather };
