3a923c96c94ec831e69f0eceb8e0f54758f68638
Alexander Fedorov <rasmygens@gmail.com>
2022-11-06 04:27:41 +0300

    done

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { getWeather } from './services/api.service.js';
-import { printHelp, printError, printSuccess } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICT } from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printError, printSuccess, printWeather } from './services/log.service.js';
+import { saveKeyValue, getKeyValue, TOKEN_DICT } from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
@@ -18,18 +18,50 @@ const saveToken = async (token) => {
 	}
 };
 
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError('Не передан город');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICT.city, city);
+		printSuccess('Город сохранен');
+	}
+	catch (e) {
+		printError(e.message);
+	}
+};
+
+
+const getForecast = async () => {
+	try {
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICT.city);
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
+	}
+	catch (e) {
+		if (e?.response?.status == 404) {
+			printError('Неверно указан город');
+		} else if (e?.response?.status == 401) {
+			printError('Неверно указан token');
+		} else {
+			printError(e.message);
+		}
+	}
+};
+
 const initCLI = () => {
 	const args = getArgs(process.argv);
-
 	if (args.h) {
-		printHelp();
+		return printHelp();
 	}
 	if (args.s) {
+		return saveCity(args.s);
 	}
 	if (args.t) {
 		return saveToken(args.t);
 	}
-	getWeather('moscow');
+	return getForecast();
 };
 
 initCLI();
